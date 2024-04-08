7ae75fdff731132d4ad1410295634d31ab498479
Ekaterina Volkova <koweevaea@gmail.com>
2024-04-08 22:01:22 +0500

    Сделала вызов footer и sidebar из отдельного файла и добавила вкладку testing

---
+++ LifeSpot/Static/Scripts/testing.js
@@ -0,0 +1,9 @@
+﻿let name = prompt("Введите ваше имя");
+alert(`${name}, приветствуем на LifeSpot! ` +
+    new Date().toLocaleString());
+
+// Получим коллекцию всех элементов страницы
+let elements = document.getElementsByTagName('*');
+
+// Выведем результат в уведомление
+alert(`Количество элементов на странице:  ${elements.length}`);
