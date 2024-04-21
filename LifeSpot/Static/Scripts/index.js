790f2b7024f2ae695129f13e72c3dc6f6c53fa5c
Ekaterina Volkova <koweevaea@gmail.com>
2024-04-21 18:31:36 +0500

    Добавила комментарии

--- LifeSpot/Static/Scripts/index.js
+++ LifeSpot/Static/Scripts/index.js
@@ -1,10 +1,16 @@
-﻿let session = new Map();
+﻿//Создадим массив для хранения информации о текущей сессии
+let session = new Map();
 
+//Добавим данные о клиенте
 session.set("userAgent", window.navigator.userAgent);
 
+//Добавим данные о фозрасте пользователя (через запрос)
 session.set("age", prompt("Пожалуйста, введите ваш возраст"));
+
+//Добавим текущие дату и время
 session.set("date", new Date().toLocaleString());
 
+//Проверим, можно ли пользователю посетить сайт
 if (session.get("age") >= 18) {
     alert(`Приветствуем на LifeSpot! ` + session.get("date"));
 }
