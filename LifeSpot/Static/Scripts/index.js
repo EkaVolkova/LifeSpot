e1e7adb66e868915235742964ad40225f15db8ce
Ekaterina Volkova <koweevaea@gmail.com>
2024-04-21 19:42:17 +0500

    Переделала функцию фильтрации контента так, чтобы она принимала как аргумент другую функцию (ту, которая вытаскивает данные, введённые пользователем).

--- LifeSpot/Static/Scripts/index.js
+++ LifeSpot/Static/Scripts/index.js
@@ -37,10 +37,20 @@ function HandleSession() {
     return session;
 }
 
-/*
- * Функция фильтрации
+/**
+ * функция получения введенного пользователем текста
  */
-function FilterContent(filterText) {
+function GetInputData() {
+    return document.getElementsByTagName('input')[0].value.toLocaleLowerCase();
+}
+
+/**
+ * Функция фильтрации
+ * @param {any} getData функция получения введенного пользователем текста
+ */
+function FilterContent(getData) {
+
+    let filterText = getData();
 
     //Получаем список всех видео на странице
     let videos = document.getElementsByClassName('video-container');
@@ -49,7 +59,7 @@ function FilterContent(filterText) {
     for (var i = 0; i < videos.length; i++) {
         //Проверяем, есть ли в названии видео символы, введенные пользователем, если есть - делаем элемент видимым, если нет - невидимым
         let videoName = videos[i].getElementsByClassName('video-title')[0].innerHTML;
-        if (videoName.toLowerCase().includes(filterText.toLowerCase()))
+        if (videoName.toLowerCase().includes(filterText))
             videos[i].style.display = 'inline-block';
         else
             videos[i].style.display = 'none';
