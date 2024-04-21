1ac1ff7e93df270039b22065bdfe5838d5b6d5f1
Ekaterina Volkova <koweevaea@gmail.com>
2024-04-21 18:46:54 +0500

    Добавила начальные проверки из скрпта в отдельную функцию

--- LifeSpot/Static/Scripts/index.js
+++ LifeSpot/Static/Scripts/index.js
@@ -1,24 +1,25 @@
-﻿//Создадим массив для хранения информации о текущей сессии
-let session = new Map();
+﻿function HandleSession() {
+    //Создадим массив для хранения информации о текущей сессии
+    let session = new Map();
 
-//Добавим данные о клиенте
-session.set("userAgent", window.navigator.userAgent);
+    //Добавим данные о клиенте
+    session.set("userAgent", window.navigator.userAgent);
 
-//Добавим данные о фозрасте пользователя (через запрос)
-session.set("age", prompt("Пожалуйста, введите ваш возраст"));
+    //Добавим данные о фозрасте пользователя (через запрос)
+    session.set("age", prompt("Пожалуйста, введите ваш возраст"));
 
-//Добавим текущие дату и время
-session.set("date", new Date().toLocaleString());
+    //Добавим текущие дату и время
+    session.set("date", new Date().toLocaleString());
 
-//Проверим, можно ли пользователю посетить сайт
-if (session.get("age") >= 18) {
-    alert(`Приветствуем на LifeSpot! ` + session.get("date"));
+    //Проверим, можно ли пользователю посетить сайт
+    if (session.get("age") >= 18) {
+        alert(`Приветствуем на LifeSpot! ` + session.get("date"));
+    }
+    else {
+        alert("Сайт предназначен для совершеннолетних!");
+        window.location.href = "http://www.google.com";
+    }
 }
-else {
-    alert("Сайт предназначен для совершеннолетних!");
-    window.location.href = "http://www.google.com";
-}
-
 /*
  * Функция фильтрации
  */
