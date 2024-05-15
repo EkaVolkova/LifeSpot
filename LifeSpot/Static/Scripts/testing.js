1c2144ea14ec80912d8b09da278f3017964e5ab1
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-05-15 09:22:21 +0500

    Рефакторинг testing.js через функцию

--- LifeSpot/Static/Scripts/testing.js
+++ LifeSpot/Static/Scripts/testing.js
@@ -1,9 +1,8 @@
-﻿let name = prompt("Введите ваше имя");
-alert(`${name}, приветствуем на LifeSpot! ` +
-    new Date().toLocaleString());
+﻿function countElements() {
 
-// Получим коллекцию всех элементов страницы
-let elements = document.getElementsByTagName('*');
+    // Получим коллекцию всех элементов страницы
+    let elements = document.getElementsByTagName('*');
 
-// Выведем результат в уведомление
-alert(`Количество элементов на странице:  ${elements.length}`);
+    // Выведем результат в уведомление
+    alert(`Количество элементов на странице:  ${elements.length}`);
+}
