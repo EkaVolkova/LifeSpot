d7db60d99750267774f8110c2be653b5c70f5fa5
VolkovaEkaterina <asu-dev007@yandex.ru>
2024-05-15 09:32:40 +0500

    Добавила функцию для сохранения контекста функции и вывода на эеран текущего и предыдущего ввода

--- LifeSpot/Static/Scripts/testing.js
+++ LifeSpot/Static/Scripts/testing.js
@@ -1,8 +1,20 @@
-﻿function countElements() {
+﻿/**
+ * Функция подсчитывает количество элементов на странице и выводит результат на экран
+ * */
+function countElements() {
 
     // Получим коллекцию всех элементов страницы
     let elements = document.getElementsByTagName('*');
 
     // Выведем результат в уведомление
     alert(`Количество элементов на странице:  ${elements.length}`);
+}
+
+/**
+ * Функция для сохранения контекста функции и вывода на эеран текущего и предыдущего ввода
+ * */
+function saveContext() {
+    let currentInput = document.getElementsByTagName('input')[0].value.toLocaleLowerCase();
+    alert(`Текущий ввод: ${currentInput}\r\nПредыдущий ввод: ${this.lastInput}`);
+    this.lastInput = currentInput;
 }
